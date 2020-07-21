996e215c411490faf736f2b4b79714c19ffe2fa3
saifur703 <saifur703@gmail.com>
2020-07-21 10:59:01 +0600

    responsive

--- assets/js/active.js
+++ assets/js/active.js
@@ -16,6 +16,35 @@
       smartSpeed: 500,
       animateOut: 'fadeOut',
     });
+
+    /*==============================
+            BOTTOM SCROLL TOP BUTTON
+        ================================*/
+
+    var scrollTop = $('.scrollTop');
+    $(window).scroll(function () {
+      var topPos = $(this).scrollTop();
+      if (topPos > 100) {
+        $(scrollTop).css('opacity', '1');
+      } else {
+        $(scrollTop).css('opacity', '0');
+      }
+    });
+    $(scrollTop).on('click', function () {
+      $('html, body').animate(
+        {
+          scrollTop: 0,
+        },
+        800
+      );
+      return false;
+    });
+
+    /*==============================
+        Slick Nav
+        ================================*/
+    var $ourmenu = $('#ourmenu');
+    $ourmenu.slicknav();
   });
 
   jQuery(window).on('load', function () {});
